409d88f31b00855b7e8c505bd80d675178da900b
Xart22 <fajar.rn22@gmail.com>
2026-02-14 12:24:57 +0700

    '2

--- rumija-service/src/server.ts
+++ rumija-service/src/server.ts
@@ -62,7 +62,7 @@ async function start() {
   const fastify = await buildServer();
 
   try {
-    await fastify.listen({ port: 3004, host: "0.0.0.0" });
+    await fastify.listen({ port: 3005, host: "0.0.0.0" });
     console.log("LaporanMasyarakat service running on http://localhost:3004");
   } catch (err) {
     fastify.log.error(err);
